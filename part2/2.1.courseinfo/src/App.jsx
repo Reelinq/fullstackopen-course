ffcbb33a75bca1078ba19e1143014c754ca77331
Boris Savushkin <savbopa@gmail.com>
2025-07-01 21:06:42 +0300

    Part 2 | 2.1 exercise

---
+++ part2/2.1.courseinfo/src/App.jsx
@@ -0,0 +1,72 @@
+const Header = ({ name }) => {
+  return(
+  <>
+    <h1>
+      {name}
+    </h1>
+  </>
+  )
+}
+
+const Content = ({ parts }) => {
+  return(
+    <div>
+      {parts.map(part => 
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      )}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return(
+      <p>Number of exercises {totalExercises}</p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
