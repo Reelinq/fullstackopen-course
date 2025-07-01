cbd525d647203797ff3358cd56f1d7af8d2f410a
Boris Savushkin <savbopa@gmail.com>
2025-07-01 11:29:48 +0300

    Part 1 | 1.8 exercise

--- part1/1.6-1.7.unicafe/src/App.jsx
+++ part1/1.6-1.7.unicafe/src/App.jsx
@@ -1,30 +1,16 @@
 import { useState } from 'react'
 
-const Header = ({ content }) => <><h1>{content}</h1></>
+const Header = ({ header }) => <><h1>{header}</h1></>
 
 const Button = ({ onClick, text }) => <button onClick={onClick} >{text}</button>
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({ header, good, bad, neutral }) => {
 
   const all = good + bad + neutral
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
-
   return (
-    <div>
-      <div id='feedback'>
-        <Header content='give feedback' />
-        <Button onClick={increaseGood} text='good' />
-        <Button onClick={increaseNeutral} text='neutral' />
-        <Button onClick={increaseBad} text='bad' />
-      </div>
-      <div id='statictics'>
-        <Header content='statistics' />
+    <div id='statistics'>
+        <Header header={header} />
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
@@ -32,7 +18,27 @@ const App = () => {
         <p>average {(good + bad * -1) / all}</p>
         <p>positive {good * 100 / all} %</p>
       </div>
-    </div>
+  )
+}
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <>
+      <div id='feedback'>
+        <Header header='give feedback' />
+        <Button onClick={increaseGood} text='good' />
+        <Button onClick={increaseNeutral} text='neutral' />
+        <Button onClick={increaseBad} text='bad' />
+      </div>
+      <Statistics header='statistics' good={good} bad={bad} neutral={neutral} />
+    </>
   )
 }
 
