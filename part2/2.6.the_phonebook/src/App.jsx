ce9c912f947e31c46e23192648ba27bdef00e71a
Boris Savushkin <savbopa@gmail.com>
2025-07-01 23:19:04 +0300

    Part 2 | Fix to the 2.6 exercise

--- part2/2.6.the_phonebook/src/App.jsx
+++ part2/2.6.the_phonebook/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1 }
+    { name: 'Arto Hellas'}
   ]) 
   const [newName, setNewName] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName,
-      id: String(persons.length + 1)
+      name: newName
     }
     
     setPersons(persons.concat(nameObject))
@@ -29,7 +28,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.id}>{person.name}</p> )}
+      {persons.map(person => <p key={person.name}>{person.name}</p> )}
     </div>
   )
 
