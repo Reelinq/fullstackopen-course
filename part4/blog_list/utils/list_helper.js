5712fb7e4ac83df01ff78d287b842bf7ad1972ed
Boris Savushkin <savbopa@gmail.com>
2025-07-06 23:32:47 +0300

    Part 4 | 4.5 exercise

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,6 +6,10 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((fav, blog) => blog.likes > fav.likes ? blog : fav)
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
