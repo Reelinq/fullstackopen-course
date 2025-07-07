9557a661cafc33d481030ff232d2216407f3e369
Boris Savushkin <savbopa@gmail.com>
2025-07-07 11:49:22 +0300

    Part 4 | 4.6 exercise

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -10,6 +12,19 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((fav, blog) => blog.likes > fav.likes ? blog : fav)
 }
 
+function mostBlogs(blogs) {
+  const groupedByAuthor = _.groupBy(blogs, 'author')
+
+  const authorsBlogCount = Object.entries(groupedByAuthor).map(([author, blogs]) => ({
+    author,
+    blogs: blogs.length
+  }))
+
+  return authorsBlogCount.reduce((max, authorObj) =>
+    authorObj.blogs > max.blogs ? authorObj : max
+  )
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
