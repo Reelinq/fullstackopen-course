c170a49c33f60e8ff9ec50a09b161e071b81bbe6
Boris Savushkin <savbopa@gmail.com>
2025-07-09 00:54:39 +0300

    Refactored App.jsx to components for easier development

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import loginService from './services/login'
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LogIn from './components/LogIn'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -24,63 +22,17 @@ const App = () => {
     }
   }, [])
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
-
-      window.localStorage.setItem(
-        'loggedBlogappUser', JSON.stringify(user)
-      ) 
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-  }  
 
   const handleLogout = async => {
     window.localStorage.removeItem('loggedBlogappUser')
     setUser(null)
   }
 
-   if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-              <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-              <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    )
-  }
+  const loginForm = () => (
+    <LogIn setUser={setUser} />
+  )
 
-  return (
+  const blogForm = () => (
     <div>
       <h2>blogs</h2>
       <span>{user.name} logged in</span>
@@ -89,6 +41,13 @@ const App = () => {
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+      {!user && loginForm()}
+      {user && blogForm()}
     </div>
   )
 }
