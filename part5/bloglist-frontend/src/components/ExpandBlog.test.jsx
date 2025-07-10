ee4e539d68b9c14e0ae93f453ca67744b31cfa9f
Boris Savushkin <savbopa@gmail.com>
2025-07-10 15:01:41 +0300

    Part 5 | 5.14 exercise

---
+++ part5/bloglist-frontend/src/components/ExpandBlog.test.jsx
@@ -0,0 +1,51 @@
+import { render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { describe, test, expect, vi } from 'vitest'
+import ExpandBlog from './ExpandBlog'
+import Blog from './Blog'
+
+describe('<ExpandBlog />', () => {
+  const blog = {
+    title: 'exampleTitle',
+    author: 'exampleAuthor',
+    url: 'exampleUrl',
+    likes: 2,
+    user: {
+      name: 'Test user',
+      username: 'testuser'
+    }
+  }
+
+  const mockOnHide = vi.fn()
+  const mockSetBlogs = vi.fn()
+
+  test('renders title when not expanded', () => {
+    render(
+      <ExpandBlog blog={blog}>
+        <Blog blog={blog} onHide={mockOnHide} blogs={[]} setBlogs={mockSetBlogs} />
+      </ExpandBlog>
+    )
+
+    const collapsedSection = screen.getByText('show').closest('span')
+    const element = within(collapsedSection).getByText('exampleTitle', { exact: false })
+    expect(element).toBeInTheDocument()
+  })
+
+  test('renders full blog info after clicking show', async () => {
+    const user = userEvent.setup()
+
+    render(
+      <ExpandBlog blog={blog}>
+        <Blog blog={blog} onHide={mockOnHide} blogs={[]} setBlogs={mockSetBlogs} />
+      </ExpandBlog>
+    )
+
+    const showButton = screen.getByText('show')
+    await user.click(showButton)
+
+    expect(screen.getAllByText('exampleTitle exampleAuthor')[1]).toBeVisible()
+    expect(screen.getByText('exampleUrl')).toBeInTheDocument()
+    expect(screen.getByText('likes 2')).toBeInTheDocument()
+    expect(screen.getByText('Test user')).toBeInTheDocument()
+  })
+})
