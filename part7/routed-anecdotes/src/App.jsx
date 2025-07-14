d2a876174776fb2d632874d4b003d2c6bc1e9353
Boris Savushkin <savbopa@gmail.com>
2025-07-14 12:22:17 +0300

    Part 7 | 7.2 exercise

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -3,50 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Menu from './components/Menu'
 import Footer from './components/Footer'
 
-
-
-
-
-
-
-const CreateNew = ({ addNew }) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    addNew({
-      content,
-      author,
-      info,
-      votes: 0
-    })
-  }
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
-
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
     {
--- part7/routed-anecdotes/src/components/Menu.jsx
+++ part7/routed-anecdotes/src/components/Menu.jsx
@@ -1,18 +1,26 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useMatch } from 'react-router-dom'
 import CreateNew from './CreateNew'
 import About from './About'
 import AnecdoteList from './AnecdoteList'
+import Anecdote from './Anecdote'
 
 const Menu = ({ addNew, anecdotes }) => {
   const padding = {
     paddingRight: 5
   }
+
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match 
+    ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
+    : null
+    
   return (
     <div>
       <Link style={padding} to="/">anecdotes</Link>
       <Link style={padding} to="/create">create new</Link>
       <Link style={padding} to="/about">about</Link>
       <Routes>
+        <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
         <Route path="/create" element={<CreateNew addNew={addNew} />} />
         <Route path="/about" element={<About />} />
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
