79ceecf810d247cdcd20f0adc921a02ac9af2b01
Boris Savushkin <savbopa@gmail.com>
2025-07-20 16:12:56 +0300

    Part 8 | 8.20 exercise

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Books from "./components/Books"
 import NewBook from "./components/NewBook"
 import SetBirthyear from "./components/SetBirthyear"
 import Login from "./components/Login"
+import Recommendations from "./components/Recommendations"
 
 const App = () => {
 	const client = useApolloClient()
@@ -32,6 +33,7 @@ const App = () => {
 					<button onClick={() => setPage("books")}>books</button>
 					<button onClick={() => setPage("add")}>add book</button>
 					<button onClick={() => setPage("setYear")}>set birthyear</button>
+					<button onClick={() => setPage("recommend")}>recommend</button>
 					<button onClick={handleLogout}>logout</button>
 				</div>
 			)}
@@ -40,6 +42,7 @@ const App = () => {
 			<Books show={page === "books"} />
 			{token && <NewBook show={page === "add"} />}
 			{token && <SetBirthyear show={page === "setYear"} />}
+			{token && <Recommendations show={page === "recommend"} />}
 			<Login show={page === "login"} setToken={setToken} setPage={setPage} />
 		</div>
 	)
---
+++ part8/library-frontend/src/components/Recommendations.jsx
@@ -0,0 +1,44 @@
+import { ALL_BOOKS, USER } from "../queries"
+import { useQuery } from "@apollo/client"
+
+const Recommendations = ({ show }) => {
+	const getUser = useQuery(USER)
+
+	const getBooks = useQuery(ALL_BOOKS, {
+		variables: { genre: getUser.data?.me?.favoriteGenre },
+		skip: !getUser.data?.me?.favoriteGenre
+	})
+
+	if (getBooks.loading || getBooks.loading) {
+		return <div>loading...</div>
+	}
+
+	const books = getBooks.data?.allBooks || []
+
+	if (!show) return null
+
+	return (
+		<div>
+			<h2>recommendations</h2>
+			<p>books in your favorite genre <b>{getUser.data.me.favoriteGenre}</b></p>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>author</th>
+						<th>published</th>
+					</tr>
+					{books.map(b => (
+						<tr key={b.title}>
+							<td>{b.title}</td>
+							<td>{b.author.name}</td>
+							<td>{b.published}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
+}
+
+export default Recommendations
