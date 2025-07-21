b64fa647cdc3a6f4640485867c2fe87b0d707829
Boris Savushkin <savbopa@gmail.com>
2025-07-21 16:52:47 +0300

    Part 9 | 9.6 exercise

--- part9/TypeScript-first-steps/bmiCalculator.ts
+++ part9/TypeScript-first-steps/bmiCalculator.ts
@@ -1,32 +1,32 @@
 const parseArgumentsBMI = (args: string[]): [number, number] => {
-	if (args.length < 4) throw new Error('Not enough arguments')
-	if (args.length > 4) throw new Error('Too many arguments')
+	if (args.length < 4) throw new Error('Not enough arguments');
+	if (args.length > 4) throw new Error('Too many arguments');
 
 	if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
 		return [
 			Number(args[2]),
 			Number(args[3])
-		]
+		];
 	} else {
 		throw new Error('Provided values were not numbers!');
 	}
-}
+};
 
 export const calculateBmi = (height: number, weight: number): string => {
-	const heightInMeters = height / 100
-	const BMI = weight / (heightInMeters ** 2)
+	const heightInMeters = height / 100;
+	const BMI = weight / (heightInMeters ** 2);
 
-	if (BMI < 16) return 'Underweight (Severe thinness)'
-	if (BMI < 17) return 'Underweight (Moderate thinness)'
-	if (BMI < 18.5) return 'Underweight (Mild thinness)'
-	if (BMI < 25) return 'Normal range'
-	if (BMI < 30) return 'Overweight (Pre-obese)'
-	if (BMI < 35) return 'Obese (Class I)'
-	if (BMI < 40) return 'Obese (Class II)'
-	return 'Obese (Class III)'
-}
+	if (BMI < 16) return 'Underweight (Severe thinness)';
+	if (BMI < 17) return 'Underweight (Moderate thinness)';
+	if (BMI < 18.5) return 'Underweight (Mild thinness)';
+	if (BMI < 25) return 'Normal range';
+	if (BMI < 30) return 'Overweight (Pre-obese)';
+	if (BMI < 35) return 'Obese (Class I)';
+	if (BMI < 40) return 'Obese (Class II)';
+	return 'Obese (Class III)';
+};
 
 if (require.main === module) {
-	const [height, weight] = parseArgumentsBMI(process.argv)
-	console.log(calculateBmi(height, weight))
+	const [height, weight] = parseArgumentsBMI(process.argv);
+	console.log(calculateBmi(height, weight));
 }
--- part9/TypeScript-first-steps/exerciseCalculator.ts
+++ part9/TypeScript-first-steps/exerciseCalculator.ts
@@ -1,15 +1,15 @@
 const parseArgumentsExerciseCaluclator = (args: string[]): [days: number[], target: number] => {
-	if (args.length < 4) throw new Error('Not enough arguments')
+	if (args.length < 4) throw new Error('Not enough arguments');
 
-	const target = Number(args[2])
-	if (isNaN(target)) throw new Error('Provided values were not numbers!')
+	const target = Number(args[2]);
+	if (isNaN(target)) throw new Error('Provided values were not numbers!');
 
-	const days = args.slice(3).map(day => Number(day))
+	const days = args.slice(3).map(day => Number(day));
 	for (const arg of days) {
-		if (isNaN(Number(arg))) throw new Error('Provided values were not numbers!')
+		if (isNaN(Number(arg))) throw new Error('Provided values were not numbers!');
 	}
-	return [days, target]
-}
+	return [days, target];
+};
 
 interface MultiplyValues {
 	periodLength: number
@@ -30,27 +30,27 @@ const calculateExercises = (days: number[], target: number): MultiplyValues => {
 		ratingDescription: 'you passed the target, well done',
 		target,
 		average: 0
-	}
+	};
 
-	let sum = 0
+	let sum = 0;
 
 	for (const day of days) {
-		if (day !== 0) MultiplyValues.trainingDays++
-		sum += day
+		if (day !== 0) MultiplyValues.trainingDays++;
+		sum += day;
 	}
 
-	MultiplyValues.average = sum / MultiplyValues.periodLength
-	MultiplyValues.success = MultiplyValues.average >= MultiplyValues.target
+	MultiplyValues.average = sum / MultiplyValues.periodLength;
+	MultiplyValues.success = MultiplyValues.average >= MultiplyValues.target;
 	if (!MultiplyValues.success) {
-		MultiplyValues.rating = 1
-		MultiplyValues.ratingDescription = 'not bad, but horrible'
+		MultiplyValues.rating = 1;
+		MultiplyValues.ratingDescription = 'not bad, but horrible';
 	} else if (MultiplyValues.average / 2 >= target) {
-		MultiplyValues.rating = 3
-		MultiplyValues.ratingDescription = 'amazing job, you are twice as good'
+		MultiplyValues.rating = 3;
+		MultiplyValues.ratingDescription = 'amazing job, you are twice as good';
 	}
 
-	return MultiplyValues
-}
+	return MultiplyValues;
+};
 
-const [days, target] = parseArgumentsExerciseCaluclator(process.argv)
-console.log(calculateExercises(days, target))
+const [days, target] = parseArgumentsExerciseCaluclator(process.argv);
+console.log(calculateExercises(days, target));
