a6c57b8916c8415e70bff9f5b2a40e8d6e0cd4dd
Boris Savushkin <savbopa@gmail.com>
2025-07-23 00:03:09 +0300

    Part 9 | 9.23 exercise

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -17,11 +17,53 @@ export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
 
 export type NewPatientEntry = z.infer<typeof newEntrySchema>;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
-}
-
 export interface PatientEntry extends NewPatientEntry {
 	id: string;
 	entries: Entry[];
 }
+
+export interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<DiagnoseEntry['code']>;
+}
+
+export interface Discharge {
+	date: string
+	criteria: string
+}
+
+export interface HospitalEntry extends BaseEntry {
+	type: "Hospital";
+	discharge: Discharge
+}
+
+export interface SickLeave {
+	startDate: string
+	endDate: string
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+	type: "OccupationalHealthcare";
+	employerName: string
+	sickLeave?: SickLeave
+}
+
+export enum HealthCheckRating {
+	"Healthy" = 0,
+	"LowRisk" = 1,
+	"HighRisk" = 2,
+	"CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+	type: "HealthCheck";
+	healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+	| HospitalEntry
+	| OccupationalHealthcareEntry
+	| HealthCheckEntry;
