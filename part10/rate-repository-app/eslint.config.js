ee14ece9a21d6fcc9f6c120a0e847bef6c491610
Boris Savushkin <savbopa@gmail.com>
2025-07-24 11:44:03 +0300

    Part 10 | 10.3 exercise
    Also changed eslint.config to a new one to make linting actually work

---
+++ part10/rate-repository-app/eslint.config.js
@@ -0,0 +1,41 @@
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactNative from 'eslint-plugin-react-native';
+import js from '@eslint/js';
+import babelParser from '@babel/eslint-parser';
+
+export default [
+	{
+		files: ['**/*.js', '**/*.jsx'],
+		languageOptions: {
+			parser: babelParser,
+			globals: {
+				__DEV__: 'readonly',
+			},
+			sourceType: 'module',
+		},
+		plugins: {
+			react: eslintPluginReact,
+			'react-native': eslintPluginReactNative,
+		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...eslintPluginReact.configs.recommended.rules,
+			'react/prop-types': 'off',
+			'react/react-in-jsx-scope': 'off',
+		},
+	},
+	{
+		languageOptions: {
+			globals: {
+				navigator: 'readonly',
+				__DEV__: 'readonly',
+			},
+		},
+		settings: {},
+	},
+];
