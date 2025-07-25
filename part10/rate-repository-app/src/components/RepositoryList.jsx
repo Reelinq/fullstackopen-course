869da931f8c27e2ca417d4f1353b2b26d266909a
Boris Savushkin <savbopa@gmail.com>
2025-07-25 23:05:08 +0300

    Part 10 | 10.23 exercise

--- part10/rate-repository-app/src/components/RepositoryList.jsx
+++ part10/rate-repository-app/src/components/RepositoryList.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { Link } from 'react-router-native';
+import { Picker } from '@react-native-picker/picker';
 
 const styles = StyleSheet.create({
 	separator: {
@@ -9,9 +11,20 @@ const styles = StyleSheet.create({
 	},
 });
 
+export const SortingPicker = ({ selectedSorting, setSelectedSorting }) => (
+	<Picker
+		selectedValue={selectedSorting}
+		onValueChange={(itemValue) => setSelectedSorting(itemValue)}
+	>
+		<Picker.Item label="Latest repositories" value="latest" />
+		<Picker.Item label="Highest rated repositories" value="highest" />
+		<Picker.Item label="Lowest rated repositories" value="lowest" />
+	</Picker>
+)
+
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+export const RepositoryListContainer = ({ repositories, selectedSorting, setSelectedSorting }) => {
 	const repositoryNodes = repositories
 		? repositories.edges.map((edge) => edge.node)
 		: [];
@@ -20,6 +33,10 @@ export const RepositoryListContainer = ({ repositories }) => {
 		<FlatList
 			data={repositoryNodes}
 			ItemSeparatorComponent={ItemSeparator}
+			keyExtractor={(item) => item.id}
+			ListHeaderComponent={() => (
+				<SortingPicker selectedSorting={selectedSorting} setSelectedSorting={setSelectedSorting} />
+			)}
 			renderItem={({ item }) => (
 				<Link to={`/repositories/${item.id}`}>
 					<RepositoryItem item={item} />
@@ -30,9 +47,27 @@ export const RepositoryListContainer = ({ repositories }) => {
 };
 
 const RepositoryList = () => {
-	const { repositories } = useRepositories();
+	const [selectedSorting, setSelectedSorting] = useState('latest');
 
-	return <RepositoryListContainer repositories={repositories} />;
+	const getSortingParams = (sorting) => {
+		switch (sorting) {
+			case 'latest': return { order: 'CREATED_AT', direction: 'DESC' };
+			case 'highest': return { order: 'RATING_AVERAGE', direction: 'DESC' };
+			case 'lowest': return { order: 'RATING_AVERAGE', direction: 'ASC' };
+			default: return { order: 'CREATED_AT', direction: 'DESC' };
+		}
+	};
+
+	const sortingParams = getSortingParams(selectedSorting);
+	const { repositories } = useRepositories(sortingParams);
+
+	return (
+		<RepositoryListContainer
+			repositories={repositories}
+			selectedSorting={selectedSorting}
+			setSelectedSorting={setSelectedSorting}
+		/>
+	)
 };
 
 export default RepositoryList;
--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,8 +1,9 @@
 import { GET_REPOSITORIES } from '../graphql/queries';
 import { useQuery } from '@apollo/client';
 
-const useRepositories = () => {
+const useRepositories = ({ order, direction }) => {
 	const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+		variables: { orderBy: order, orderDirection: direction },
 		fetchPolicy: 'cache-and-network'
 	});
 
