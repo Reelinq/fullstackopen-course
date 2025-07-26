f7f5cd7cfcc778532f7d7446dbe7ca527df9f355
Boris Savushkin <savbopa@gmail.com>
2025-07-26 21:41:34 +0300

    Removed all the tests since when doing commit, those would be checked anyway

--- part11/pokedex/build_step.sh
+++ part11/pokedex/build_step.sh
@@ -2,19 +2,6 @@
 
 echo "Build script"
 
-echo "Installing dependencies"
 npm install
-echo "Installing playwright"
-npx playwright install --with-deps
 
-echo "Running eslint"
-npm run eslint
-
-echo "Running build"
 npm run build
-
-echo "Running tests"
-npm test
-
-echo "Running e2e tests"
-npm run test:e2e
