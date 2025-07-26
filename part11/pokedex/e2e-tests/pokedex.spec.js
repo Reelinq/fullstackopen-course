35509de3e95e5f1c873a164a783623f083228d18
Boris Savushkin <savbopa@gmail.com>
2025-07-26 20:31:17 +0300

    Added another test to e2e testing

--- part11/pokedex/e2e-tests/pokedex.spec.js
+++ part11/pokedex/e2e-tests/pokedex.spec.js
@@ -6,4 +6,13 @@ describe('Pokedex', () => {
 		await expect(page.getByText('ivysaur')).toBeVisible()
 		await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
 	})
+	test('pokemon page can be navigated to', async ({ page }) => {
+		await page.goto('')
+		await expect(page.getByText('ivysaur')).toBeVisible()
+		await page.getByText('ivysaur').click()
+		await expect(page.getByText('Home')).toBeVisible()
+		await expect(page.getByText('Next')).toBeVisible()
+		await expect(page.getByText('Previous')).toBeVisible()
+		await expect(page.getByText('ivysaur')).toBeVisible()
+	})
 })
