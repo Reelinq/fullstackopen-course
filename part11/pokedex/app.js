9a2acdc84e789223c81bc56c7da592c109426652
Boris Savushkin <savbopa@gmail.com>
2025-07-27 12:04:07 +0300

    Updated healthCheck to check every 5 minutes

--- part11/pokedex/app.js
+++ part11/pokedex/app.js
@@ -4,18 +4,11 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
+app.use(express.static('dist'))
+
 app.get('/health', (req, res) => {
 	res.send('ok')
 })
-
-app.get('*', (req, res) => {
-	if (req.path !== '/health') {
-		process.exit(1) // Crashes the server
-	}
-	res.status(404).send('Not found')
-})
-
-app.use(express.static('dist'))
 
 app.listen(PORT, () => {
 	// eslint-disable-next-line no-console
