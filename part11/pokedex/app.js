3330572ed67af9ee9beb7fc14086589510d15edf
Boris Savushkin <savbopa@gmail.com>
2025-07-27 11:32:14 +0300

    Manually adding errors

--- part11/pokedex/app.js
+++ part11/pokedex/app.js
@@ -7,10 +7,16 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-	// eslint-disable-next-line no-constant-condition
-	if (true) throw ('error...  ')
 	res.send('ok')
 })
+
+app.get('*', (req, res) => {
+	if (req.path !== '/health') {
+		process.exit(1); // Crashes the server
+	}
+	res.status(404).send('Not found');
+});
+
 app.listen(PORT, () => {
 	// eslint-disable-next-line no-console
 	console.log(`server started on port ${PORT}`)
