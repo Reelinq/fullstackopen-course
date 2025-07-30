375d465677993a564e8e03df6a90926cb963849b
Boris Savushkin <savbopa@gmail.com>
2025-07-30 15:44:40 +0300

    Part 13 | 13.13 exercise

--- part13/notes-postgres-app/controllers/blogs.js
+++ part13/notes-postgres-app/controllers/blogs.js
@@ -1,14 +1,24 @@
 const router = require('express').Router()
 const tokenExtractor = require('../middleware/tokenExtractor')
+const { Op } = require('sequelize')
 
 const { Blog, User } = require('../models')
 
+
+
 router.get('/', async (req, res) => {
+	const where = {}
+	if (req.query.search) {
+		where.title = {
+			[Op.substring]: req.query.search
+		}
+	}
 	const blogs = await Blog.findAll({
 		attributes: { exclude: ['userId'] },
 		include: {
 			model: User
-		}
+		},
+		where
 	})
 	res.json(blogs)
 })
