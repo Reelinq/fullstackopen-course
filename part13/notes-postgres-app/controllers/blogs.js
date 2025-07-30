0dd339f7637ce1a91740325ff74ef86eabc1ff5a
Boris Savushkin <savbopa@gmail.com>
2025-07-30 15:45:44 +0300

    Part 13 | 13.14 exercise

--- part13/notes-postgres-app/controllers/blogs.js
+++ part13/notes-postgres-app/controllers/blogs.js
@@ -9,9 +9,10 @@ const { Blog, User } = require('../models')
 router.get('/', async (req, res) => {
 	const where = {}
 	if (req.query.search) {
-		where.title = {
-			[Op.substring]: req.query.search
-		}
+		where[Op.or] = [
+			{ title: { [Op.substring]: req.query.search } },
+			{ author: { [Op.substring]: req.query.search } }
+		]
 	}
 	const blogs = await Blog.findAll({
 		attributes: { exclude: ['userId'] },
