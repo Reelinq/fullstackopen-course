2523ff100477bac9940667b4b08827e19d3787a8
Boris Savushkin <savbopa@gmail.com>
2025-07-30 15:02:14 +0300

    Part 13 | 13.8 and 13.9 exercises

--- part13/notes-postgres-app/controllers/blogs.js
+++ part13/notes-postgres-app/controllers/blogs.js
@@ -1,14 +1,35 @@
 const router = require('express').Router()
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
 
 router.get('/', async (req, res) => {
-	const blogs = await Blog.findAll()
+	const blogs = await Blog.findAll({
+		attributes: { exclude: ['userId'] },
+		include: {
+			model: User,
+			attributes: ['name']
+		}
+	})
 	res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
-	const blog = await Blog.create(req.body)
+const tokenExtractor = (req, res, next) => {
+	const authorization = req.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		try {
+			req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+		} catch {
+			return res.status(401).json({ error: 'token invalid' })
+		}
+	} else {
+		return res.status(401).json({ error: 'token missing' })
+	}
+	next()
+}
+
+router.post('/', tokenExtractor, async (req, res) => {
+	const user = await User.findByPk(req.decodedToken.id)
+	const blog = await Blog.create({ ...req.body, userId: user.id, date: new Date() })
 	return res.json(blog)
 })
 
--- part13/notes-postgres-app/models/index.js
+++ part13/notes-postgres-app/models/index.js
@@ -1,7 +1,15 @@
 const Blog = require('./blog')
+const User = require('./user')
 
-Blog.sync()
+User.hasMany(Blog)
+Blog.belongsTo(User)
+const syncModels = async () => {
+	await User.sync({ alter: true })
+	await Blog.sync({ alter: true })
+}
+
+syncModels()
 
 module.exports = {
-	Blog
+	Blog, User
 }
